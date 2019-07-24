dc13df92b545fdd7ae267a6b468578cc9cc01f0d
Edwin Salinas Parra <edwinsalespa@gmail.com>
2019-07-24 03:03:08 -0500

    crea una vista con el nombre de platzi_people de la tabla people de la base de datos platziBlog

--- platziBlog.sql
+++ platziBlog.sql
@@ -15,6 +15,31 @@ city varchar(255) null,
 constraint primary key (person_id)
 );
 
+insert into platziblog.people (last_name,first_name,address,city)
+value ("salinas", "edwin", "calle1", "medellin");
+
+insert into platziblog.people (last_name,first_name,address,city)
+value ("salinas", "alex", "carrera 2", "pitalito");
+
+insert into platziblog.people (last_name,first_name,address,city)
+value ("hernandez", "yenny", "carrera al cielo", "medellin");
+
+/*muestra la tabla people*/
 select * from platziblog.people;
+
+/*crea una vista con el nombre de platzi_people de la tabla people de la base de datos platziBlog*/
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = root@localhost 
+    SQL SECURITY DEFINER
+VIEW platziblog.platzi_people AS
+    SELECT 
+        platziblog.people.person_id AS person_id,
+        platziblog.people.last_name AS last_name,
+        platziblog.people.first_name AS first_name,
+        platziblog.people.address AS address,
+        platziblog.people.city AS city
+    FROM
+        platziblog.people
 
 
