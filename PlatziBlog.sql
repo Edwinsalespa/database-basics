2ee9420f2c283c16256bb6c1de73c1d3a4d1d1e1
Edwin Salinas Parra <edwinsalespa@gmail.com>
2019-08-02 23:31:08 -0500

    tablas terminadas e insercion de datos a las tablas

--- PlatziBlog.sql
+++ PlatziBlog.sql
@@ -12,12 +12,30 @@ nom_categoria varchar(30) not null,
 constraint primary key (id)
 );
 
+insert categorias (nom_categoria)
+value   ("programacion"),
+		("ciencia"),
+        ("tecnologia"),
+		("base de datos"),
+        ("git");
+        
+select * from categorias;
+
 create table etiquetas
 (
 id int not null auto_increment,
 nom_etiquetas varchar(30) not null,
 constraint primary key (id)
 );
+
+insert etiquetas (nom_etiquetas)
+value   ("curso de programacion"),
+		("cura con el cancer"),
+        ("nuevo tesla"),
+		("NoSQL"),
+        ("git en android");
+        
+select * from etiquetas;
 
 create table usuarios 
 (
@@ -30,7 +48,16 @@ create table usuarios
   unique key email_unique (email)
 );
 
-create table post
+/*insercion en la tabla usuarios*/
+insert usuarios (login, pasword, nickname, email)
+value   ("edwinsalespa",1234,"edwinsalespa", "edwinsalespa@gmail.com"),
+		("alexsalespa",1234,"alexsalespa", "alexsalespa@gmail.com"),
+        ("robinsalespa",1234,"robinsalespa", "robinsalespa@gmail.com");
+        
+select * from usuarios;
+
+/*Tablas dependientes*/
+create table pots
 (
   id int not null auto_increment,
   titulo varchar(130) not null,
@@ -43,26 +70,79 @@ create table post
   constraint foreign key (categoria_id) references categorias (id) on delete no action on update no action,
   constraint foreign key (usuario_id) references usuarios (id) on delete no action on update cascade
   );
+  
+  insert into pots (titulo, fecha_publicacion, contenido, estatus,usuario_id,categoria_id)
+  value   ("platziblog",'2019-07-16 00:00:00',"proyrcto de fundamentos de base de datos", "activo",1,1),
+		  ("platziblog2",'2019-05-12 00:00:00',"proyrcto de fundamentos de base de datos", "activo",2,2),
+          ("platziblog3",'2019-07-23 00:00:00',"proyrcto de fundamentos de base de datos", "activo",3,3);
+          
+select * from pots;
+select * from etiquetas;
 
 create table comentarios
 (
   id int not null auto_increment,
   cuerpo_comentario text not null,
   usuario_id int not null,
-  post_id int not null,
-  primary key (id),
-  constraint foreign key (post_id) references post (id) on delete no action on update no action,
+  pots_id int not null,
+  constraint primary key (id),
+  constraint foreign key (pots_id) references pots (id) on delete no action on update no action,
   constraint foreign key (usuario_id) references usuarios (id) on delete no action on update no action
 );
 
+insert comentarios (cuerpo_comentario, usuario_id, pots_id)
+value ("la mejor educacion",1,5),
+	  ("la mejor educacion",2,6);
+
+select * from comentarios;
+
+/*enlace de muchos a muchos en las tablas post y etiquetas*/
 create table pots_etiquetas
 (
-post_id int not null,
+pots_id int not null,
 etiquetas_id int not null,
-constraint foreign key (post_id) references post (id) on delete no action on update no action,
+constraint foreign key (pots_id) references pots (id) on delete no action on update no action,
 constraint foreign key (etiquetas_id) references etiquetas (id) on delete no action on update no action,
-constraint primary key (post_id,etiquetas_id)
+constraint primary key (pots_id,etiquetas_id)
 );
+
+insert pots_etiquetas (pots_id, etiquetas_id)
+value (5,1),
+	  (6,1),
+      (7,1);
+      
+select * from pots_etiquetas;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
